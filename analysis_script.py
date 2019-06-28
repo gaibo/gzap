d48a64a9fad760802e0593cee258a66ea1610fa9
gaibo <gzhang@cboe.com>
2019-06-28 18:21:24 -0400

    initial push

---
+++ analysis_script.py
@@ -0,0 +1,232 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from itertools import zip_longest
+from sklearn.linear_model import LinearRegression
+import seaborn as sns
+
+from model.data_structures import ETF, Index, VolatilityIndex
+
+
+def share_dateindex(timeseries_list):
+    column_list = range(len(timeseries_list))
+    combined_df = pd.DataFrame(dict(zip(column_list, timeseries_list))).dropna()
+    return [combined_df[column].rename('value') for column in column_list]
+
+
+def make_basicstatstable(instr_list):
+    table_out = pd.DataFrame()
+    for instr in instr_list:
+        prices = instr.price()
+        pct_changes = instr.price_return(logarithmic=False)
+        instr_table = \
+            pd.concat([pd.DataFrame({instr.name: [prices.first_valid_index(),
+                                                  prices.last_valid_index()]},
+                                    index=['start date', 'end date']).T,
+                       prices.describe().rename(instr.name).iloc[0:1].to_frame().T.astype(int),
+                       prices.describe().rename(instr.name).iloc[1:].to_frame().T,
+                       pct_changes.describe().rename(instr.name).iloc[1:].to_frame().T
+                       .add_suffix(' change (%)') * 100],
+                      axis=1)
+        table_out = table_out.append(instr_table)
+    return table_out
+
+
+def make_lineplot(data_list, label_list=None, xlabel=None, ylabel=None, title=None, ax=None):
+    # Prepare labels
+    if label_list is None:
+        label_list = len(data_list) * ['']
+    # Prepare Figure and Axes
+    if ax is None:
+        fig, ax = plt.subplots()
+    else:
+        fig = None
+    # Create lines
+    for data, label in zip_longest(data_list, label_list):
+        ax.plot(data, label=label, linewidth=3)
+    # Configure
+    ax.legend()
+    ax.grid(True)
+    # Set labels
+    if xlabel:
+        ax.set_xlabel(xlabel)
+    if ylabel:
+        ax.set_ylabel(ylabel)
+    if title:
+        ax.set_title(title)
+    return fig, ax
+
+
+def make_fillbetween(x, y1, y2=0, label=None, xlabel=None, ylabel=None, title=None, ax=None):
+    # Prepare Figure and Axes
+    if ax is None:
+        fig, ax = plt.subplots()
+    else:
+        fig = None
+    # Create filled line plot
+    ax.fill_between(x, y1, y2, color='g', label=label)
+    ax.legend()
+    ax.grid(True)
+    # Set labels
+    if xlabel:
+        ax.set_xlabel(xlabel)
+    if ylabel:
+        ax.set_ylabel(ylabel)
+    if title:
+        ax.set_title(title)
+    return fig, ax
+
+
+def make_scatterplot(x_data, y_data, do_center=False,
+                     xlabel=None, ylabel=None, title=None, ax=None):
+    # Prepare Figure and Axes
+    if ax is None:
+        fig, ax = plt.subplots()
+    else:
+        fig = None
+    # Create scatter
+    [joined_x_data, joined_y_data] = share_dateindex([x_data, y_data])
+    ax.scatter(joined_x_data, joined_y_data)
+    # Create best fit line
+    x = joined_x_data.values.reshape(-1, 1)
+    y = joined_y_data.values
+    model = LinearRegression(fit_intercept=False).fit(x, y)
+    ax.plot(joined_x_data, model.coef_*joined_x_data, 'k')
+    # Configure
+    ax.grid(True)
+    if do_center:
+        lim = max(max(joined_x_data.abs()), max(joined_y_data.abs()))
+        ax.set_xlim(-lim, lim)
+        ax.set_ylim(-lim, lim)
+    # Set labels
+    if xlabel:
+        ax.set_xlabel(xlabel)
+    if ylabel:
+        ax.set_ylabel(ylabel)
+    if title:
+        ax.set_title(title)
+    return fig, ax
+
+
+def make_histogram(data, hist=True, n_bins=10, line=True,
+                   label=None, xlabel=None, ylabel=None, title=None, ax=None):
+    # Prepare Figure and Axes
+    if ax is None:
+        fig, ax = plt.subplots()
+    else:
+        fig = None
+    # Create distribution histogram
+    if hist:
+        ax.hist(data, bins=n_bins, density=True, label=label)
+    # Create density plot line
+    if line:
+        if isinstance(data, list):
+            print("ERROR: can't make multiple density plot lines at same time.")
+        else:
+            sns.distplot(data, hist=False, ax=ax, label=label)
+    # Configure
+    ax.legend()
+    ax.grid(True)
+    # Set labels
+    if xlabel:
+        ax.set_xlabel(xlabel)
+    if ylabel:
+        ax.set_ylabel(ylabel)
+    if title:
+        ax.set_title(title)
+    return fig, ax
+
+
+def main():
+    # Import data sources with pandas
+    full_data = pd.read_csv('data/index_data.csv', index_col='Date', parse_dates=True)
+    eurostoxx_data = pd.read_csv('data/STOXX50E.csv', index_col='Date', parse_dates=True)
+    sptr_data = pd.read_csv('data/sptr_vix.csv', index_col='Date', parse_dates=True)
+
+    # Create data objects
+    spx = Index(sptr_data['SPTR'], 'SPX')
+    vix = VolatilityIndex(full_data['VIX.Index'], spx, 'VIX')
+    hyg = ETF(full_data['HYG.US.Equity'], 'HYG')
+    vxhyg = VolatilityIndex(full_data['VXHYG.Index'], hyg, 'VXHYG')
+    ief = ETF(full_data['IEF.US.Equity'], 'IEF')  # 7-10 year treasury bond ETF
+    vxief = VolatilityIndex(full_data['VXHYG.Index'], ief, 'VXIEF')
+    sx5e = Index(eurostoxx_data['Close'], 'Euro Stoxx 50')
+    vstoxx = VolatilityIndex(full_data['V2X.Index'], sx5e, 'VSTOXX')
+    agg = ETF(full_data['AGG.US.Equity'], 'AGG')
+    lqd = ETF(full_data['LQD.US.Equity'], 'LQD')
+    jgbvix = VolatilityIndex(full_data['SPJGBV.Index'], None, 'JGB VIX')    # Need futures data
+    tyvix = VolatilityIndex(full_data['TYVIX.Index'], None, 'TYVIX')  # Need futures data
+
+    # Name some truncated timeseries
+    start_date = '2004-01-01'
+    end_date = '2018-01-01'
+    truncd_vix = vix.price().truncate(start_date, end_date)
+    truncd_spx = spx.price().truncate(start_date, end_date)
+    truncd_agg = agg.price().truncate(start_date, end_date)
+    truncd_lqd = lqd.price().truncate(start_date, end_date)
+    truncd_jgbvix = jgbvix.price().truncate(start_date, end_date)
+    truncd_tyvix = tyvix.price().truncate(start_date, end_date)
+
+    # Example data processing that can be done with framework
+
+    # Line Plots
+    make_lineplot([truncd_spx/truncd_spx[0], truncd_agg/truncd_agg[0], truncd_jgbvix/truncd_jgbvix[0],
+                   truncd_lqd/truncd_lqd[0], truncd_tyvix/truncd_tyvix[0]],
+                  ['SPX', 'AGG'],
+                  ylabel='Normalized Level', title='SPX and AGG')
+
+    # Tables
+    example_table = make_basicstatstable([spx, vix, hyg, vxhyg, ief, vxief, sx5e, vstoxx]).round(1)
+    print(example_table)
+
+    # Scatterplots
+    make_scatterplot(spx.price_return(logarithmic=False), vix.price_return(logarithmic=False), False,
+                     '% Change in SPX', '% Change in VIX', '% Change: SPX vs. VIX')
+
+    # Line Plots with Difference
+    truncd_realvol = 100 * vix.undl_realized_vol(do_shift=True).truncate(start_date, end_date)
+    [joined_vix, joined_realvol] = share_dateindex([truncd_vix, truncd_realvol])
+    fig, axs = plt.subplots(2, 1, sharex='all')
+    make_lineplot([joined_vix, joined_realvol],
+                  ['VIX Level', 'SPX Realized Vol (21 Days Shifted)'], ax=axs[0])
+    difference = joined_vix - joined_realvol
+    make_fillbetween(difference.index, joined_vix, joined_realvol,
+                     label='Difference', ax=axs[0])
+    make_fillbetween(difference.index, difference, label='Difference', ax=axs[1])
+
+    # Histograms
+    [joined_vxhyg, joined_realhygvol] = share_dateindex([vxhyg.price(),
+                                                         vxhyg.undl_realized_vol(do_shift=True) * 100])
+    hyg_voldiff = joined_vxhyg - joined_realhygvol
+    fig, ax = make_histogram([difference, hyg_voldiff], hist=True, n_bins=100, line=False,
+                             label=['VIX', 'VXHYG'], xlabel='Vol Points', title='Risk Premium')
+    make_histogram(difference, hist=False, line=True, label='VIX', ax=ax)
+    make_histogram(hyg_voldiff, hist=False, line=True, label='VXHYG', ax=ax)
+
+    # Matrix Analysis
+    fig, axs = plt.subplots(6, 6)
+    instr_list = [spx, vix, hyg, ief, sx5e, agg]
+    for row, instr_x in zip(range(5, -1, -1), instr_list):
+        for col, instr_y in zip(range(6), instr_list):
+            if col > (5-row):
+                # Top triangle
+                make_scatterplot(instr_x.price_return(), instr_y.price_return(), ax=axs[row, col])
+            elif (5-row) > col:
+                # Bottom triangle
+                [joined_x_data, joined_y_data] = share_dateindex([instr_x.price_return(),
+                                                                  instr_y.price_return()])
+                x = joined_x_data.values.reshape(-1, 1)
+                y = joined_y_data.values
+                model = LinearRegression(fit_intercept=False).fit(x, y)
+                r_sq = round(model.score(x, y), 2)
+                slope = round(model.coef_[0], 2)
+                axs[row, col].text(0.35, 0.4, "Slope: {}\nR^2: {}".format(slope, r_sq))
+            else:
+                # Diagonal
+                make_histogram(instr_x.price_return(), hist=True, n_bins=100, line=False,
+                               ax=axs[row, col])
+
+    return 0
+
+
+if __name__ == '__main__':
+    main()
---
+++ model/data_structures.py
@@ -0,0 +1,251 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+BUS_DAYS_IN_MONTH = 21
+BUS_DAYS_IN_YEAR = 252
+ONE_DAY = pd.Timedelta(days=1)
+ONE_NANOSECOND = pd.Timedelta(nanoseconds=1)
+
+
+def construct_timeseries(ts_df):
+    """ Construct pseudo-time-series object, i.e. a pandas Series with 'time' and 'value'
+    :param ts_df: DataFrame object with 1 index (time) and 1 column (value)
+    :return: pd.Series object with 'time' as index and 'value' as name
+    """
+    ts_df = ts_df.reset_index()
+    assert ts_df.shape[1] == 2   # 2 columns - time and value
+    ts = pd.Series(ts_df.iloc[:, 1].values, index=ts_df.iloc[:, 0], name='value')
+    ts.index.name = 'time'
+    ts = ts.sort_index()
+    return ts.dropna()
+
+
+class Instrument(object):
+    """
+    Financial instrument base class
+    """
+    def __init__(self, ts_df, name='', tradestats=pd.DataFrame(None)):
+        """
+        :param ts_df: time-series DataFrame with time and value
+        :param name: name of the data
+        :param tradestats: (optional) trade statistics to be included, as a DataFrame
+        """
+        self.levels = construct_timeseries(ts_df)
+        self.name = name
+        self.tradestats = tradestats
+
+    def price(self, granularity='daily', time_start=None, time_end=None,
+              intraday_interval='5T', multiday_interval='M'):
+        """ Output price levels with custom granularity
+        :param granularity: 'daily', 'intraday', 'multiday', or None
+        :param time_start: start of time-series to use
+        :param time_end: end of time-series to use
+        :param intraday_interval: interval to use with 'intraday'
+        :param multiday_interval: interval to use with 'multiday'
+        :return: pd.Series with 'time' and 'value'
+        """
+        # Get specified date/time range
+        if time_start is None:
+            time_start = self.levels.first_valid_index()
+        if time_end is None:
+            time_end = self.levels.last_valid_index()
+        truncd_levels = self.levels.truncate(time_start, time_end)
+        # Return prices based on granularity choice
+        if granularity == 'daily':
+            days = truncd_levels.index.normalize().unique()
+            if len(days) == len(truncd_levels.index):
+                # We already have daily data
+                return truncd_levels
+            else:
+                # Create daily data
+                return construct_timeseries(
+                           pd.DataFrame([truncd_levels[day:day+ONE_DAY-ONE_NANOSECOND].iloc[-1]
+                                         for day in days], index=days))
+        elif granularity == 'intraday':
+            # NOTE: currently unsure if we need to upsample (pad()) or downsample (mean()) or both
+            if time_start != time_end:
+                print("WARNING: only time_start parameter is used for intraday.")
+            intraday_day = truncd_levels[time_start:time_start+ONE_DAY-ONE_NANOSECOND]
+            day_upsample_pad = intraday_day.resample('S').pad()
+            day_downsample_mean = day_upsample_pad.resample(intraday_interval,
+                                                            label='right', closed='right').mean()
+            return day_downsample_mean
+        elif granularity == 'multiday':
+            # NOTE: perhaps this should be combined with 'intraday'
+            return truncd_levels.resample(multiday_interval, label='right', closed='right').mean()
+        else:
+            return truncd_levels
+
+    def price_return(self, logarithmic=True,
+                     granularity='daily', time_start=None, time_end=None,
+                     intraday_interval='5T', multiday_interval='M'):
+        """ Calculate percent or logarithmic returns with custom granularity
+        :param logarithmic: set False for percent returns
+        :param granularity: 'daily', 'intraday', 'multiday', or None
+        :param time_start: start of time-series to use
+        :param time_end: end of time-series to use
+        :param intraday_interval: interval to use with 'intraday'
+        :param multiday_interval: interval to use with 'multiday'
+        :return: pd.Series with 'time' and 'value'
+        """
+        prices = self.price(granularity, time_start, time_end,
+                            intraday_interval, multiday_interval)
+        if logarithmic:
+            return np.log(prices).diff().iloc[1:]
+        else:
+            return prices.pct_change().iloc[1:]
+
+    def realized_vol(self, do_shift=False):
+        """ Calculate annualized realized vol from past month
+        :param do_shift: set True to shift data back one month, to compare to implied vol
+        :return: pd.Series with 'time' and 'value', with ~20 NaNs at beginning
+        """
+        result = np.sqrt(self.price_return().rolling(BUS_DAYS_IN_MONTH).var(ddof=0)
+                         * BUS_DAYS_IN_YEAR)
+        if do_shift:
+            return result.shift(-BUS_DAYS_IN_MONTH)
+        else:
+            return result
+
+
+class CashInstr(Instrument):
+    """
+    Cash instrument, derived from financial instrument
+    """
+    def __init__(self, ts_df, name='', tradestats=pd.DataFrame(None)):
+        """
+        :param ts_df: time-series DataFrame with time and value
+        :param name: name of the data
+        :param tradestats: (optional) trade statistics to be included, as a DataFrame
+        """
+        super().__init__(ts_df, name, tradestats)
+
+
+class Derivative(Instrument):
+    """
+    Derivative instrument, derived from financial instrument
+    """
+    def __init__(self, ts_df, underlying, name='', tradestats=pd.DataFrame(None)):
+        """
+        :param ts_df: time-series DataFrame with time and value
+        :param underlying: underlying Instrument
+        :param name: name of the data
+        :param tradestats: (optional) trade statistics to be included, as a DataFrame
+        """
+        super().__init__(ts_df, name, tradestats)
+        self.underlying = underlying
+
+    def undl_realized_vol(self, do_shift=False):
+        """ Calculate annualized realized vol from past month for underlying asset
+        :param do_shift: set True to shift data back one month, to compare to implied vol
+        :return: pd.Series with 'time' and 'value', with ~20 NaNs at beginning
+        """
+        return self.underlying.realized_vol(do_shift)
+
+
+class Index(CashInstr):
+    def __init__(self, ts_df, name=''):
+        """
+        :param ts_df: time-series DataFrame with time and value
+        :param name: name of the data
+        """
+        super().__init__(ts_df, name)
+
+
+class Stock(CashInstr):
+    def __init__(self, ts_df, name='', tradestats=pd.DataFrame(None)):
+        """
+        :param ts_df: time-series DataFrame with time and value
+        :param name: name of the data
+        :param tradestats: (optional) trade statistics to be included, as a DataFrame
+        """
+        super().__init__(ts_df, name, tradestats)
+
+
+class ETF(CashInstr):
+    def __init__(self, ts_df, name='', tradestats=pd.DataFrame(None)):
+        """
+        :param ts_df: time-series DataFrame with time and value
+        :param name: name of the data
+        :param tradestats: (optional) trade statistics to be included, as a DataFrame
+        """
+        super().__init__(ts_df, name, tradestats)
+
+
+class Futures(Derivative):
+    def __init__(self, ts_df, underlying, maturity, name='', tradestats=pd.DataFrame(None)):
+        """
+        :param ts_df: time-series DataFrame with time and value
+        :param underlying: underlying Instrument
+        :param maturity: maturity date of futures
+        :param name: name of the data
+        :param tradestats: (optional) trade statistics to be included, as a DataFrame
+        """
+        super().__init__(ts_df, underlying, name, tradestats)
+        self.maturity = maturity
+
+
+class Options(Derivative):
+    def __init__(self, ts_df, underlying, expiry, pc, strike, name='', tradestats=pd.DataFrame(None)):
+        """
+        :param ts_df: time-series DataFrame with time and value
+        :param underlying: underlying Instrument
+        :param expiry: expiration date of options contract
+        :param pc: whether options contract is put or call
+        :param strike: strike price of options contract
+        :param name: name of the data
+        :param tradestats: (optional) trade statistics to be included, as a DataFrame
+        """
+        super().__init__(ts_df, underlying, name, tradestats)
+        self.expiry = expiry
+        self.pc = pc
+        self.strike = strike
+
+
+class VolatilityIndex(Index):
+    def __init__(self, ts_df, underlying, name=''):
+        """
+        :param ts_df: time-series DataFrame with time and value
+        :param underlying: the underlying instrument whose volatility is being gauged
+        :param name: name of the data
+        """
+        super().__init__(ts_df, name)
+        self.underlying = underlying
+
+    def undl_realized_vol(self, do_shift=False):
+        """ Calculate annualized realized vol from past month for underlying asset
+        :param do_shift: set True to shift data back one month, to compare to implied vol
+        :return: pd.Series with 'time' and 'value', with ~20 NaNs at beginning
+        """
+        return self.underlying.realized_vol(do_shift)
+
+
+def main():
+    # Load example data
+    sptr_vix_df = pd.read_csv('sptr_vix.csv')
+    sptr_vix_df['Date'] = pd.to_datetime(sptr_vix_df['Date'])
+    sptr_vix_df = sptr_vix_df.set_index('Date')
+
+    # Create objects
+    sptr = Index(sptr_vix_df['SPTR'])
+    vix = VolatilityIndex(sptr_vix_df['VIX'], sptr)
+
+    # Look at implied volatility vs realized volatility
+    start = pd.Timestamp('2015-01-01')
+    end = pd.Timestamp('2018-01-01')
+    truncd_vix = vix.price().truncate(start, end)
+    truncd_realized_vol = vix.undl_realized_vol().truncate(start, end)
+    truncd_shifted_realized_vol = vix.undl_realized_vol(do_shift=True).truncate(start, end)
+
+    fig, ax = plt.subplots()
+    ax.plot(truncd_vix, label='VIX', linewidth=3)
+    ax.plot(100 * truncd_realized_vol, label='SPTR Realized Vol')
+    ax.plot(100 * truncd_shifted_realized_vol, label='SPTR Realized Vol from 21 Days Ahead', linewidth=3)
+    ax.legend()
+
+    return 0
+
+
+if __name__ == '__main__':
+    main()
