ba1b53696535475eb70d6c61a7626acb9258fedd
gaibo <gzhang@cboe.com>
2019-06-28 15:34:14 -0400

    added histograms to analysis script

--- analysis_script.py
+++ analysis_script.py
@@ -2,6 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from itertools import zip_longest
 from sklearn.linear_model import LinearRegression
+import seaborn as sns
 
 from data_structures import ETF, Index, VolatilityIndex
 
@@ -10,6 +11,24 @@ def share_dateindex(timeseries_list):
     column_list = range(len(timeseries_list))
     combined_df = pd.DataFrame(dict(zip(column_list, timeseries_list))).dropna()
     return [combined_df[column].rename('value') for column in column_list]
+
+
+def make_basicstatstable(instr_list):
+    table_out = pd.DataFrame()
+    for instr in instr_list:
+        prices = instr.price()
+        pct_changes = instr.price_return(logarithmic=False)
+        instr_table = \
+            pd.concat([pd.DataFrame({instr.name: [prices.first_valid_index(),
+                                                  prices.last_valid_index()]},
+                                    index=['start date', 'end date']).T,
+                       prices.describe().rename(instr.name).iloc[0:1].to_frame().T.astype(int),
+                       prices.describe().rename(instr.name).iloc[1:].to_frame().T,
+                       pct_changes.describe().rename(instr.name).iloc[1:].to_frame().T
+                       .add_suffix(' change (%)') * 100],
+                      axis=1)
+        table_out = table_out.append(instr_table)
+    return table_out
 
 
 def make_lineplot(data_list, label_list=None, xlabel=None, ylabel=None, title=None, ax=None):
@@ -37,7 +56,7 @@ def make_lineplot(data_list, label_list=None, xlabel=None, ylabel=None, title=No
     return fig, ax
 
 
-def make_fillbetween(x, y1, y2=0, label='', xlabel=None, ylabel=None, title=None, ax=None):
+def make_fillbetween(x, y1, y2=0, label=None, xlabel=None, ylabel=None, title=None, ax=None):
     # Prepare Figure and Axes
     if ax is None:
         fig, ax = plt.subplots()
@@ -57,7 +76,8 @@ def make_fillbetween(x, y1, y2=0, label='', xlabel=None, ylabel=None, title=None
     return fig, ax
 
 
-def make_scatterplot(x_data, y_data, do_center=False, xlabel=None, ylabel=None, title=None, ax=None):
+def make_scatterplot(x_data, y_data, do_center=False,
+                     xlabel=None, ylabel=None, title=None, ax=None):
     # Prepare Figure and Axes
     if ax is None:
         fig, ax = plt.subplots()
@@ -87,22 +107,30 @@ def make_scatterplot(x_data, y_data, do_center=False, xlabel=None, ylabel=None, 
     return fig, ax
 
 
-def make_basicstatstable(instr_list):
-    table_out = pd.DataFrame()
-    for instr in instr_list:
-        prices = instr.price()
-        pct_changes = instr.price_return(logarithmic=False)
-        instr_table = \
-            pd.concat([pd.DataFrame({instr.name: [prices.first_valid_index(),
-                                                  prices.last_valid_index()]},
-                                    index=['start date', 'end date']).T,
-                       prices.describe().rename(instr.name).iloc[0:1].to_frame().T.astype(int),
-                       prices.describe().rename(instr.name).iloc[1:].to_frame().T,
-                       pct_changes.describe().rename(instr.name).iloc[1:].to_frame().T
-                       .add_suffix(' change (%)') * 100],
-                      axis=1)
-        table_out = table_out.append(instr_table)
-    return table_out
+def make_histogram(data, hist=True, n_bins=10, line=True,
+                   label=None, xlabel=None, ylabel=None, title=None, ax=None):
+    # Prepare Figure and Axes
+    if ax is None:
+        fig, ax = plt.subplots()
+    else:
+        fig = None
+    # Create distribution histogram
+    if hist:
+        ax.hist(data, bins=n_bins, density=True, label=label)
+    # Create density plot line
+    if line:
+        sns.distplot(data, hist=False, ax=ax, label=label)
+    # Configure
+    ax.legend()
+    ax.grid(True)
+    # Set labels
+    if xlabel:
+        ax.set_xlabel(xlabel)
+    if ylabel:
+        ax.set_ylabel(ylabel)
+    if title:
+        ax.set_title(title)
+    return fig, ax
 
 
 def main():
@@ -125,14 +153,19 @@ def main():
     jgbvix = VolatilityIndex(full_data['SPJGBV.Index'], None, 'JGB VIX')    # Need futures data
     tyvix = VolatilityIndex(full_data['TYVIX.Index'], None, 'TYVIX')  # Need futures data
 
-    # Execute organized commands to create desired analyses
+    # Name some truncated timeseries
+    start_date = '2004-01-01'
+    end_date = '2018-01-01'
+    truncd_vix = vix.price().truncate(start_date, end_date)
+    truncd_spx = spx.price().truncate(start_date, end_date)
+    truncd_agg = agg.price().truncate(start_date, end_date)
+    truncd_lqd = lqd.price().truncate(start_date, end_date)
+    truncd_jgbvix = jgbvix.price().truncate(start_date, end_date)
+    truncd_tyvix = tyvix.price().truncate(start_date, end_date)
+
+    # Example data processing that can be done with framework
 
     # Line Plots
-    truncd_spx = spx.price().truncate('2004-01-01', '2018-01-01')
-    truncd_agg = agg.price().truncate('2004-01-01', '2018-01-01')
-    truncd_lqd = lqd.price().truncate('2004-01-01', '2018-01-01')
-    truncd_jgbvix = jgbvix.price().truncate('2004-01-01', '2018-01-01')
-    truncd_tyvix = tyvix.price().truncate('2004-01-01', '2018-01-01')
     make_lineplot([truncd_spx/truncd_spx[0], truncd_agg/truncd_agg[0], truncd_jgbvix/truncd_jgbvix[0],
                    truncd_lqd/truncd_lqd[0], truncd_tyvix/truncd_tyvix[0]],
                   ['SPX', 'AGG'],
@@ -143,12 +176,11 @@ def main():
     print(example_table)
 
     # Scatterplots
-    make_scatterplot(spx.price_return(False), vix.price_return(False), False,
+    make_scatterplot(spx.price_return(logarithmic=False), vix.price_return(logarithmic=False), False,
                      '% Change in SPX', '% Change in VIX', '% Change: SPX vs. VIX')
 
     # Line Plots with Difference
-    truncd_vix = vix.price().truncate('2004-01-01', '2018-01-01')
-    truncd_realvol = 100 * vix.undl_realized_vol(do_shift=True).truncate('2004-01-01', '2018-01-01')
+    truncd_realvol = 100 * vix.undl_realized_vol(do_shift=True).truncate(start_date, end_date)
     [joined_vix, joined_realvol] = share_dateindex([truncd_vix, truncd_realvol])
     fig, axs = plt.subplots(2, 1, sharex='all')
     make_lineplot([joined_vix, joined_realvol],
@@ -159,6 +191,13 @@ def main():
     make_fillbetween(difference.index, difference, label='Difference', ax=axs[1])
 
     # Histograms
+    [joined_vxhyg, joined_realhygvol] = share_dateindex([vxhyg.price(),
+                                                         vxhyg.undl_realized_vol(do_shift=True) * 100])
+    hyg_voldiff = joined_vxhyg - joined_realhygvol
+    fig, ax = make_histogram([difference, hyg_voldiff], hist=True, n_bins=100, line=False,
+                             label=['VIX', 'VXHYG'], xlabel='Vol Points', title='Risk Premium')
+    make_histogram(difference, hist=False, line=True, label='VIX', ax=ax)
+    make_histogram(hyg_voldiff, hist=False, line=True, label='VXHYG', ax=ax)
 
     return 0
 
