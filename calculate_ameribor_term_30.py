b79e155f2479f193f2d3ec7366165bde63477520
Zhang <gzhang@cboe.com>
2021-06-09 14:44:21 -0500

    checkpoint in calculate_ameribor_term_30.py: finish version 2 with Peter Goldman corrections; waiting on him for further evaluation on discrepancy to Bloomberg

--- calculate_ameribor_term_30.py
+++ calculate_ameribor_term_30.py
@@ -90,11 +90,15 @@ def apply_helper_instrument(s):
 
 
 afx_data['instrument'] = afx_data['trade_id'].apply(apply_helper_instrument)
+# Filter 0: drop duplicates - 2 records of some trades
+afx_data = afx_data.drop_duplicates('trade_id')
 # Filter 1: throw out busted trades, assert funded
 afx_data = afx_data[afx_data['busted_at'].isna() & afx_data['funded_at'].notna()]
 # Filter 2: overnight or 30-day lending markets only
 afx_data = afx_data[(afx_data['instrument'] == 'overnight_unsecured_ameribor_loan')
-                    | (afx_data['instrument'] == 'thirty_day_unsecured_ameribor_loan')]
+                    | (afx_data['instrument'] == 'thirty_day_unsecured_ameribor_loan')
+                    | (afx_data['instrument'] == 'bilateral_loan_overnight')
+                    | (afx_data['instrument'] == 'direct_settlement_loan_overnight')]
 # Create crucial columns matching DTCC format
 # NOTE: required logic for correct days to maturity: find next business day AFTER incrementing by 1 or 30
 afx_data['Trade Date'] = afx_data['funded_at'].apply(lambda tstz: tstz.replace(tzinfo=None)).dt.normalize()
@@ -158,94 +162,153 @@ combined_data = combined_data.sort_values(['Date', 'DBPV', 'Principal Amount'],
                                           ascending=[True, False, False]).reset_index(drop=True)
 
 ####
+# Run the AMBOR30T calculation process
 
-# Temp 2021 test
-# test = combined_data.set_index('Date').loc['2021-01-04':'2021-03-19']
+# Determine dataset
 test = combined_data.set_index('Date').loc['2016-01-15':'2021-03-19']
-test_dates = test.index.unique()
-# test_daily_totals = test.groupby('Date')['DBPV'].sum()
-# test['Daily Total DBPV'] = test_daily_totals
-# test['Transaction DBPV Weight'] = test['DBPV'] / test['Daily Total DBPV']
-# test['Transaction Weighted Interest Rate'] = test['Transaction DBPV Weight'] * test['Interest Rate']
+test_dates = test.index.unique()    # No idea how this compares to our trading calendars
+
+
+def filter_outliers_from_rate(data_df, filter_rate):
+    # Let through all AFX transactions OR DTCC transactions that are not outside 250bp bounds
+    filtered_data_df = \
+        data_df[~((data_df['Product Type'] == 'CP')
+                  | (data_df['Product Type'] == 'CD'))
+                | ~((data_df['Interest Rate'] > filter_rate + 2.5)
+                    | (data_df['Interest Rate'] < filter_rate - 2.5))].copy()
+    return filtered_data_df
+
+
+# For each date:
+#   1) Use previous AMBOR30T rate +-250bp to filter latest date's transactions
+#   2) Record eligible transactions into permanent dictionary;
+#      also record "isolated" daily total DBPV for easy lookback weighting purposes
+#   3) Calculate "isolated" date rate;
+#      apply $25bn minimum principal threshold to determine number of lookback days (if beyond 5) and
+#      calculate final latest AMBOR30T rate with 5+ days lookback
+AMBOR30T_DICT = {}
+ELIGIBLE_TRANSACTIONS_DF = pd.DataFrame()   # DataFrame for easy range lookup
+ISOLATED_DAILY_TOTAL_DBPV_DICT = {}
+ISOLATED_DAILY_RATE_DICT = {}
+ISOLATED_DAILY_TOTAL_VOLUME_DICT = {}
+OUTLIER_FILTER_IMPOSSIBLES = []
+OUTLIER_FILTER_APPLIEDS = []
 VOLUME_THRESHOLD_NOT_METS = []
-SAFEGUARD_NEEDEDS = []
-TERM_30_RATES_FIRSTPASS = []
-TERM_30_RATES = []
-DAY_DF_DICT_FIRSTPASS = {}
-DAY_DF_DICT = {}
-for i, (five_days_ago, today) in enumerate(zip(test_dates[:-4], test_dates[4:])):
+AMBOR30T_IMPOSSIBLES = []
+AMBOR30T_INPUT_DF_DICT = {}
+for i, today in enumerate(test_dates):
     print(today.strftime('%Y-%m-%d'))
 
-    # Test set of 5 days for volume threshold, extending beyond 5 as needed to meet minimum
-    test_curr_five_days = test.loc[five_days_ago:today].copy()
-    curr_five_day_volume = test_curr_five_days['Principal Amount'].sum()
-    if curr_five_day_volume < 25e9:
-        failed_five_day_volume = curr_five_day_volume
+    # Look up latest date's data
+    today_data = test.loc[today].copy()
+    print("\tData loaded")
+
+    # Look up previous AMBOR30T rate and filter latest transactions
+    yesterday_i = i - 1
+    if yesterday_i < 0 or test_dates[yesterday_i] not in AMBOR30T_DICT:
+        # Edge case: no previous AMBOR30T rate - cannot apply filter
+        OUTLIER_FILTER_IMPOSSIBLES.append(today)
+        print("\tWARNING: Cannot apply outlier filter - no yesterday AMBOR30T")
+    else:
+        yesterday = test_dates[yesterday_i]
+        yesterday_rate = AMBOR30T_DICT[yesterday]
+        today_data_filtered = filter_outliers_from_rate(today_data, yesterday_rate)
+        n_filtered = today_data.shape[0] - today_data_filtered.shape[0]
+        if n_filtered != 0:
+            OUTLIER_FILTER_APPLIEDS.append((today, n_filtered))
+            print(f"\tOutlier filter effective! {n_filtered} transactions removed")
+        today_data = today_data_filtered
+    ELIGIBLE_TRANSACTIONS_DF = ELIGIBLE_TRANSACTIONS_DF.append(today_data)
+    ISOLATED_DAILY_TOTAL_DBPV_DICT[today] = today_data['DBPV'].sum()/360/10000
+    ISOLATED_DAILY_TOTAL_VOLUME_DICT[today] = today_data['Principal Amount'].sum()
+
+    # Apply $25bn moveable left bound date and calculate AMBOR30T rate
+    left_bound_i = i - 4    # Initial planned lookback is 5 days
+    if left_bound_i < 0:
+        print("\tWARNING: Don't have 5 days of data yet... aborting")
+        AMBOR30T_IMPOSSIBLES.append(today)
+        continue    # Edge case at beginning of history: don't have 5 days of data yet
+    left_bound_date = test_dates[left_bound_i]
+    print(f"\tInitial lookback: {left_bound_date.strftime('%Y-%m-%d')}")
+    # Extend if needed
+    curr_bounded_data = ELIGIBLE_TRANSACTIONS_DF.loc[left_bound_date:today].copy()
+    curr_bounded_volume = curr_bounded_data['Principal Amount'].sum()
+    if curr_bounded_volume < 25e9:
+        print("\tVolume threshold extension effective!")
+        failed_five_day_volume = curr_bounded_volume
         n_days_extended = 0
         extend_success = False
-        while curr_five_day_volume < 25e9:
+        while curr_bounded_volume < 25e9:
             n_days_extended += 1
-            new_date_idx = i - n_days_extended
-            if new_date_idx < 0:
+            new_left_bound_i = left_bound_i - n_days_extended
+            if new_left_bound_i < 0:
                 # Fail: ran out of dates to extend
-                TERM_30_RATES.append((today, None))
                 VOLUME_THRESHOLD_NOT_METS.append((today, failed_five_day_volume, None, None))
+                print("\tWARNING: Principal volume could not be extended to $25bn... aborting")
                 break
-            test_curr_five_days = test.loc[test_dates[new_date_idx]:today].copy()
-            curr_five_day_volume = test_curr_five_days['Principal Amount'].sum()
+            new_left_bound_date = test_dates[new_left_bound_i]
+            curr_bounded_data = ELIGIBLE_TRANSACTIONS_DF.loc[new_left_bound_date:today].copy()
+            curr_bounded_volume = curr_bounded_data['Principal Amount'].sum()
         else:
             # Success: reaching here means volume was successfully extended
             extend_success = True
-            VOLUME_THRESHOLD_NOT_METS.append((today, failed_five_day_volume, n_days_extended, curr_five_day_volume))
+            VOLUME_THRESHOLD_NOT_METS.append((today, failed_five_day_volume, n_days_extended, curr_bounded_volume))
+            print(f"\tInitial: ${failed_five_day_volume:,.0f}\n"
+                  f"\tDays Extended: {n_days_extended}\n"
+                  f"\tFinal: ${curr_bounded_volume:,.0f}")
         if not extend_success:
-            continue    # Just move on to next date, skipping process
-
-    # Re-sort for 5-day window
-    test_curr_five_days = test_curr_five_days.sort_values(['DBPV', 'Principal Amount'], ascending=[False, False])
+            AMBOR30T_IMPOSSIBLES.append(today)
+            continue  # Just move on to next date, skipping calculation
+    # Re-sort for 5+-day window
+    curr_bounded_data = curr_bounded_data.sort_values(['DBPV', 'Principal Amount'], ascending=[False, False])
     # Calculate total DBPV
-    test_curr_five_days['5-Day Total DBPV'] = test_curr_five_days['DBPV'].sum()
+    curr_bounded_data['5+-Day Total DBPV'] = curr_bounded_data['DBPV'].sum()
     # Calculate each transaction's DBPV weight
-    test_curr_five_days['Transaction DBPV Weight'] = \
-        test_curr_five_days['DBPV'] / test_curr_five_days['5-Day Total DBPV']
+    curr_bounded_data['Transaction DBPV Weight'] = \
+        curr_bounded_data['DBPV'] / curr_bounded_data['5+-Day Total DBPV']
     # Calculate each transaction's weighted interest rate
-    test_curr_five_days['Transaction Weighted Interest Rate'] = \
-        test_curr_five_days['Transaction DBPV Weight'] * test_curr_five_days['Interest Rate']
-    # Calculate AMERIBOR Term-30 rate - FIRST PASS
-    term_30_rate_firstpass = test_curr_five_days['Transaction Weighted Interest Rate'].sum()
-    # Safeguard - check for transactions outside of 250bp range from first pass rate
-    test_curr_five_days_safeguard = \
-        test_curr_five_days[~((test_curr_five_days['Interest Rate'] > term_30_rate_firstpass + 2.5)
-                              | (test_curr_five_days['Interest Rate'] < term_30_rate_firstpass - 2.5))].copy()
-    # Calculate SECOND PASS
-    test_curr_five_days_safeguard['5-Day Total DBPV'] = test_curr_five_days_safeguard['DBPV'].sum()
-    test_curr_five_days_safeguard['Transaction DBPV Weight'] = \
-        test_curr_five_days_safeguard['DBPV'] / test_curr_five_days_safeguard['5-Day Total DBPV']
-    test_curr_five_days_safeguard['Transaction Weighted Interest Rate'] = \
-        test_curr_five_days_safeguard['Transaction DBPV Weight'] * test_curr_five_days_safeguard['Interest Rate']
-    term_30_rate = test_curr_five_days_safeguard['Transaction Weighted Interest Rate'].sum()
-    if term_30_rate_firstpass != term_30_rate:
-        SAFEGUARD_NEEDEDS.append((today, test_curr_five_days.shape[0]-test_curr_five_days_safeguard.shape[0],
-                                  term_30_rate_firstpass, term_30_rate))
-    TERM_30_RATES_FIRSTPASS.append((today, term_30_rate_firstpass))
-    TERM_30_RATES.append((today, term_30_rate))
-    DAY_DF_DICT_FIRSTPASS[today] = test_curr_five_days
-    DAY_DF_DICT[today] = test_curr_five_days_safeguard
-test_rates_firstpass = (pd.DataFrame(TERM_30_RATES_FIRSTPASS, columns=['Date', 'Term-30 Rate (First Pass)'])
-                        .set_index('Date'))
-test_rates = pd.DataFrame(TERM_30_RATES, columns=['Date', 'Term-30 Rate']).set_index('Date')
+    curr_bounded_data['Transaction Weighted Interest Rate'] = \
+        curr_bounded_data['Transaction DBPV Weight'] * curr_bounded_data['Interest Rate']
+    # Calculate AMERIBOR Term-30 rate
+    ambor30t_rate = curr_bounded_data['Transaction Weighted Interest Rate'].sum()
+    AMBOR30T_INPUT_DF_DICT[today] = curr_bounded_data
+    AMBOR30T_DICT[today] = ambor30t_rate
+
+    # For alternate calculation method: record "isolated" single-day AMBOR30T for weighting
+    today_data['Daily Total DBPV'] = today_data['DBPV'].sum()
+    today_data['Daily Transaction DBPV Weight'] = today_data['DBPV'] / today_data['Daily Total DBPV']
+    today_data['Daily Transaction Weighted Interest Rate'] = \
+        today_data['Daily Transaction DBPV Weight'] * today_data['Interest Rate']
+    isolated_daily_ambor30t_rate = today_data['Daily Transaction Weighted Interest Rate'].sum()
+    ISOLATED_DAILY_RATE_DICT[today] = isolated_daily_ambor30t_rate
+
+# Organize results into summary DataFrames
+test_rates = pd.Series(AMBOR30T_DICT).sort_index()
+test_rates.index.name, test_rates.name = 'Date', 'Replicated AMBOR30T'
 threshold_info = (pd.DataFrame(VOLUME_THRESHOLD_NOT_METS, columns=['Date', 'Failed 5-Day Volume',
                                                                    'Days Extended', 'Extended Volume'])
                   .set_index('Date'))
-safeguard_info = (pd.DataFrame(SAFEGUARD_NEEDEDS, columns=['Date', 'Transactions Omitted',
-                                                           'First Pass Rate', 'Second Pass Rate'])
-                  .set_index('Date'))
+outlier_info = (pd.DataFrame(OUTLIER_FILTER_APPLIEDS, columns=['Date', 'Transactions Omitted'])
+                .set_index('Date'))
+daily_total_dbpv = pd.Series(ISOLATED_DAILY_TOTAL_DBPV_DICT).sort_index()
+daily_rate = pd.Series(ISOLATED_DAILY_RATE_DICT).sort_index()
+daily_total_volume = pd.Series(ISOLATED_DAILY_TOTAL_VOLUME_DICT).sort_index()
 
 ####
 
 EXPORT_DIR = Path('C:/Users/gzhang/Downloads/Ameribor/Term-30 Legal Filing/')
 
 # Export input data for 6 months
-past_six_months_dates = test.loc['2020-09-01':'2021-03-19'].index.unique()
+past_six_months_dates = ELIGIBLE_TRANSACTIONS_DF.loc['2020-09-01':'2021-03-19'].index.unique()
 for date in past_six_months_dates:
     export_loc = EXPORT_DIR / '6 Months Input Data' / f'{date.strftime("%Y-%m-%d")}_dtccafx_ambor30t_input.csv'
-    DAY_DF_DICT[date].drop(['borrower_name', 'lender_name'], axis=1).to_csv(export_loc)
+    AMBOR30T_INPUT_DF_DICT[date].drop(['borrower_name', 'lender_name'], axis=1).to_csv(export_loc)
+
+DOWNLOADS_DIR = Path('C:/Users/gzhang/Downloads/')
+
+# Export input data for full history
+full_dates = ELIGIBLE_TRANSACTIONS_DF.loc['2016-06-01':'2021-03-19'].index.unique()
+for date in full_dates:
+    export_loc = (DOWNLOADS_DIR / 'My AMBOR30T Full History Input Data' /
+                  f'{date.strftime("%Y-%m-%d")}_dtccafx_ambor30t_input.csv')
+    AMBOR30T_INPUT_DF_DICT[date].to_csv(export_loc)
