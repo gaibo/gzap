f5bfc99fe959b128d8f53f13d62a5c65ecf5e1d9
Gaibo Zhang <gaibozhang@yahoo.com>
2023-11-20 14:33:17 -0500

    flesh out nike_size_scraper with more descriptive text; I might add adjustable scrape frequency next, might not because that's dangerous

--- nike_size_scraper/nike_size_scraper.py
+++ nike_size_scraper/nike_size_scraper.py
@@ -2,29 +2,33 @@ from requests_html import HTMLSession
 import pandas as pd
 import random
 from threading import Event
-from signal import SIGTERM, SIGINT, signal
+import signal
+import ctypes
 
 if __name__ == '__main__':
     # Prompt for user input
-    raw_input = \
+    input_raw = \
         input("Enter the Nike product link to monitor for stock...\n"
+              "     - Script refreshes stock availability every ~2 hours\n"
               "     - Press <Enter> to start with a default example\n"
               "...>> ")
-    if raw_input == '':
+    if input_raw == '':
         NIKE_LINK = "https://www.nike.com/t/air-trainer-1-mens-shoes-0vx2ft/DM0521-100"     # Air Trainer "Chlorophyll"
         # NIKE_LINK = "https://www.nike.com/t/air-max-90-se-mens-shoes-sXJXK0/DV2614-100"     # Air Max 90 "Moving Co"
         print(f"Querying default (Air Trainer 1 \"Chlorophyll\" - {NIKE_LINK}), please wait...")
     else:
-        NIKE_LINK = raw_input
+        NIKE_LINK = input_raw
         print("Querying above link, please wait...")
 
     # Create requests_html session
     # TODO: I prefer the "going down, going up, going sideways" recursive tree flexibility of BeautifulSoup;
     #       I know requests_html uses BS and probably has the functionality, I just can't find it; so I
     #       may rewrite this at some point with Selenium+BS instead
-    session = HTMLSession()
+    SESSION = HTMLSession()
+    # Set up collection variables
     COLLECTION_DATAFRAME = pd.DataFrame()   # Initialize storage DF
-    pd.options.display.expand_frame_repr = False
+    pd.options.display.expand_frame_repr = False    # Make DataFrames print fully
+    FIRST_TIME_THROUGH = True   # Get human-readable shoe info, only once
 
     # Repeatedly scrape Nike for sizes
     FINISH_SCRAPING = Event()   # Set up interrupting event to end infinite loop; NOTE: doesn't work on Windows...
@@ -33,31 +37,32 @@ if __name__ == '__main__':
         print(f"Interrupted by {signum}; finishing scrape...")
         FINISH_SCRAPING.set()
 
-    signal(SIGTERM, finish_handler)
-    signal(SIGINT, finish_handler)
-    FIRST_TIME_THROUGH = True   # Print human-readable shoe info, only once
+    signal.signal(signal.SIGTERM, finish_handler)
+    signal.signal(signal.SIGINT, finish_handler)  # In theory (not on Windows), program handles Ctrl+c to exit cleanly
     while not FINISH_SCRAPING.is_set():
         # Send request, get response
-        nike_response = session.get(NIKE_LINK)
+        nike_response = SESSION.get(NIKE_LINK)
         # Render Nike's extremely complicated JavaScript (this is the time-intensive part)
         # NOTE: stock cannot be ascertained without rendering JavaScript
         nike_response.html.render(retries=8, wait=3, scrolldown=3, timeout=30)  # Very finicky, maybe anti-bot
         curr_time = pd.Timestamp('now')     # Time of rendering, i.e. when site "loads
         if FIRST_TIME_THROUGH:
-            # Print human-readable shoe info, only once
+            # Print shoe info and add it to window title
             title = nike_response.html.find("h1#pdp_product_title")[0].text
             color_desc = nike_response.html.find("div ul li.description-preview__color-description.ncss-li")[0].text
-            print(f"Product found: {title}, \"{color_desc.replace('Shown: ', '')}\"")
+            product = f"{title}, \"{color_desc.replace('Shown: ', '')}\""   # Nike Air Trainer 1, "White/Medium Grey"
+            print(f"Product found: {product}")
+            ctypes.windll.kernel32.SetConsoleTitleW(f"{product} - nike_size_scraper")   # Set window title
             FIRST_TIME_THROUGH = False
 
         # Find product's sizes and check which sizes are disabled/enabled
         # NOTE: these CSS selectors are tailored from inspecting Nike in browser;
         #       initially I tried to find the sizes section using class="mt2-sm css hzulvp", but found that changes;
         #       currently find input elements (each corresponding to a size) containing "skuAndSize" attribute;
-        #       CSS selectors ":disabled" and ":enabled" can be used to determine stock, but I choose to grab
+        #       CSS selectors ":disabled" and ":enabled" can be used to determine stock-ness, but I just grab
         #       all sizes in original web page order to serve as collection DataFrame columns
-        # TODO: you would have to click the enabled (in-stock) sizes to trigger the JavaScript that tells
-        #       you whether something is low-stock; that could be a fun future feature, with Selenium
+        # TODO: you would have to click the enabled (in-stock) sizes to trigger the JavaScript that generates
+        #       text revealing something is low-stock; that could be a fun future feature, with Selenium
         sizes_input_elements = nike_response.html.find("div input[name=\"skuAndSize\"]")    # List of ~17 for shoes
         all_sizes_raw = [element.attrs['value'].split(':')[-1] for element in sizes_input_elements]
         try:
@@ -91,4 +96,7 @@ if __name__ == '__main__':
 
     # Cleanup
     print("Closing HTMLSession...")
-    session.close()
+    SESSION.close()
+    input("\nProgram exited cleanly. As of 2023-11-20, you cannot arrive at this message on Windows.\n"
+          "     - Press <Enter> to close window\n"
+          "...>> ")
