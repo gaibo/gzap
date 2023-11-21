302e8ecfa14cc941868f2acd8ab346aaa89ad236
Gaibo Zhang <gaibozhang@yahoo.com>
2023-11-20 22:38:12 -0500

    overhaul nike_size_scraper to have demand_input() - it should be visually nice now; assuming no bugs, this might be it

--- nike_size_scraper/nike_size_scraper.py
+++ nike_size_scraper/nike_size_scraper.py
@@ -4,21 +4,59 @@ import random
 from threading import Event
 import signal
 import ctypes
+from types import FrameType
 
-if __name__ == '__main__':
-    # Prompt for user input
+DEFAULT_PRODUCT_LINK = "https://www.nike.com/t/air-trainer-1-mens-shoes-0vx2ft/DM0521-100"  # Air Trainer "Chlorophyll"
+# DEFAULT_PRODUCT_LINK = "https://www.nike.com/t/air-max-90-se-mens-shoes-sXJXK0/DV2614-100"    # Air Max 90 "Moving Co"
+DEFAULT_REFRESH_MINUTES = 120
+REFRESH_DANGER_BOUND = 10   # Don't want to get banned querying too often
+
+
+def demand_input() -> tuple[str, float]:
+    """ Helper function to handle user input cases:
+        1) > 2 arguments: loop and demand inputs again
+        2) 2 arguments: parse as Nike link and refresh rate
+        3) 1 argument: parse as Nike link, use default refresh
+        4) 0 arguments: use defaults
+        NOTE: refresh rate inputs below REFRESH_DANGER_BOUND will be rejected
+    :return: tuple of (Nike website link, refresh rate in minutes)
+    """
     input_raw = \
         input("Enter the Nike product link to monitor for stock...\n"
-              "     - Script refreshes stock availability every ~2 hours\n"
-              "     - Press <Enter> to start with a default example\n"
+              "     - Optional: follow it with whitespace and a number to serve as refresh rate in minutes\n"
+              "     - e.g.: https://www.nike.com/t/air-trainer-1-mens-shoes-0vx2ft/DM0521-100 120\n"
+              "     - Press <Enter> to start with above default example\n"
               "...>> ")
-    if input_raw == '':
-        NIKE_LINK = "https://www.nike.com/t/air-trainer-1-mens-shoes-0vx2ft/DM0521-100"     # Air Trainer "Chlorophyll"
-        # NIKE_LINK = "https://www.nike.com/t/air-max-90-se-mens-shoes-sXJXK0/DV2614-100"     # Air Max 90 "Moving Co"
-        print(f"Querying default (Air Trainer 1 \"Chlorophyll\" - {NIKE_LINK}), please wait...")
+    input_list = input_raw.split()
+    if len(input_list) > 2:
+        print(f"ERROR: {len(input_list)} inputs detected instead of 1 (link) or 2 (link + refresh)", end='\n\n')
+        return demand_input()   # Restart until appropriate inputs received... it's called "demand_input"
+    elif len(input_list) == 2:
+        nike_link = input_list[0]
+        refresh_mins = float(input_list[1])     # Chance to raise ValueError
+    elif len(input_list) == 1:
+        nike_link = input_list[0]
+        refresh_mins = DEFAULT_REFRESH_MINUTES
     else:
-        NIKE_LINK = input_raw
-        print("Querying above link, please wait...")
+        # Case of just pressing <Enter>, i.e. '' empty string
+        nike_link, refresh_mins = DEFAULT_PRODUCT_LINK, DEFAULT_REFRESH_MINUTES
+        print(f"Default product: Air Trainer 1 \"Chlorophyll\" - {DEFAULT_PRODUCT_LINK}; "
+              f"Default refresh: {DEFAULT_REFRESH_MINUTES} minutes")
+    if refresh_mins < REFRESH_DANGER_BOUND:
+        print(f"Refresh rate of {refresh_mins} minutes will get both of us in trouble. Try {DEFAULT_REFRESH_MINUTES}.")
+        refresh_mins = DEFAULT_REFRESH_MINUTES
+    return nike_link, refresh_mins
+
+
+def finish_handler(signum: signal.Signals | int, _frame: FrameType) -> None:
+    # Handler function used by signal.signal() to "set" threading.Event and exit cleanly
+    print(f"Interrupted by {signum}; finishing scrape...")
+    FINISH_SCRAPING.set()
+
+
+if __name__ == '__main__':
+    input_nike_link, input_refresh_mins = demand_input()
+    print(f"Querying above link every {input_refresh_mins} minutes, please wait...")
 
     # Create requests_html session
     # TODO: I prefer the "going down, going up, going sideways" recursive tree flexibility of BeautifulSoup;
@@ -31,17 +69,12 @@ if __name__ == '__main__':
     FIRST_TIME_THROUGH = True   # Get human-readable shoe info, only once
 
     # Repeatedly scrape Nike for sizes
-    FINISH_SCRAPING = Event()   # Set up interrupting event to end infinite loop; NOTE: doesn't work on Windows...
-
-    def finish_handler(signum, _frame):
-        print(f"Interrupted by {signum}; finishing scrape...")
-        FINISH_SCRAPING.set()
-
+    FINISH_SCRAPING = Event()   # Set up interrupting event to end scraping loop; NOTE: doesn't work on Windows...
     signal.signal(signal.SIGTERM, finish_handler)
     signal.signal(signal.SIGINT, finish_handler)  # In theory (not on Windows), program handles Ctrl+c to exit cleanly
     while not FINISH_SCRAPING.is_set():
         # Send request, get response
-        nike_response = SESSION.get(NIKE_LINK)
+        nike_response = SESSION.get(input_nike_link)
         # Render Nike's extremely complicated JavaScript (this is the time-intensive part)
         # NOTE: stock cannot be ascertained without rendering JavaScript
         nike_response.html.render(retries=8, wait=3, scrolldown=3, timeout=30)  # Very finicky, maybe anti-bot
@@ -86,15 +119,17 @@ if __name__ == '__main__':
             if not diff.empty:
                 print("\nALERT! Sizing stock change:")
                 if (diff == 0).any():
-                    print(f"\tOut of stock: {list(diff[diff == 0].index)}\n")
+                    print(f"\tOut of stock: {list(diff[diff == 0].index)}")
                 if (diff == "In-Stock").any():
-                    print(f'\tIn stock: {list(diff[diff == "In-Stock"].index)}\n')
+                    print(f'\tIn stock: {list(diff[diff == "In-Stock"].index)}')
+                print(end='\n')
         # TODO: ask for email, ask for desired size(s), send email?
 
-        # Wait for approximately 2 hours (+- 15 minute "human error" range)
-        FINISH_SCRAPING.wait(2*60*60 + random.uniform(-15*60, 15*60))
+        # Wait (interruptible blocking, in theory) for approximately the refresh time (+- random "human error" range)
+        FINISH_SCRAPING.wait(input_refresh_mins*60
+                             + random.uniform(-input_refresh_mins/8*60, input_refresh_mins/8*60))
 
-    # Cleanup
+    # Cleanup, upon signal handler "setting" threading.Event to finish above loop
     print("Closing HTMLSession...")
     SESSION.close()
     input("\nProgram exited cleanly. As of 2023-11-20, you cannot arrive at this message on Windows.\n"
